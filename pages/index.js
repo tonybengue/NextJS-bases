2cdad488c71fd8841e43e2080fbc8de6dbee2c9d
Tony Bengué <tonybengue@hotmail.fr>
2022-06-28 12:19:01 +0000

    Add swr way to fetch profile

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
 import Link from "next/link"
 import { useState, useEffect } from 'react'
+// import useSWR from 'swr'
 
 function Profile1() {
   const [data, setData] = useState(null)
@@ -20,6 +21,38 @@ function Profile1() {
 
   if (isLoading) return <p>Loading...</p>
   if (!data) return <p>No profile data</p>
+
+  return (
+    <div className='profile'>
+      <h1>{data.username}</h1>
+      <p>{data.first_name}</p>
+      <div className="profile-img">
+        <img src={data.avatar}></img>
+      </div>
+      <p>{data.last_name}</p>
+      <style jsx>{`
+        .profile {
+          background-color: red;
+        }
+
+        .profile-img {
+          width: 100px;
+          border-radius: 50%;
+          background: white;
+          padding: 1rem;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+// With swr
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+function Profile2() {
+  const { data, error } = useSWR('https://random-data-api.com/api/users/random_user?size=1', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
 
   return (
     <div className='profile'>
