8b28af7b2738e252cbef0da58472d14ef84efc9b
Tony Bengué <tonybengue@hotmail.fr>
2022-06-28 12:17:59 +0000

    Add useState and useEffect to fetch profiles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,49 @@
 import Head from 'next/head'
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
+import Link from "next/link"
+import { useState, useEffect } from 'react'
+
+function Profile1() {
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch('https://random-data-api.com/api/users/random_user?size=1')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data[0])
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+  if (!data) return <p>No profile data</p>
+
+  return (
+    <div className='profile'>
+      <h1>{data.username}</h1>
+      <p>{data.first_name}</p>
+      <div className="profile-img">
+        <img src={data.avatar}></img>
+      </div>
+      <p>{data.last_name}</p>
+      <style jsx>{`
+        .profile {
+          background-color: red;
+        }
+
+        .profile-img {
+          width: 100px;
+          border-radius: 50%;
+          background: white;
+          padding: 1rem;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
